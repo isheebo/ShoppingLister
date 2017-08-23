e9721dbc54b9135da3c47fe982a29665426e5cdb
isheebo <kedgar751@gmail.com>
2017-08-23 04:53:28 +0300

    Add create_shoppinglist method

--- models/user.py
+++ models/user.py
@@ -11,3 +11,13 @@ class User:
         # a mapping of ShoppingList IDs to ShoppingList names
         self.shoppinglist_names = dict()
         self.shoppinglists = dict()  # a mapping of shopping list items
+
+    def create_shoppinglist(self, shop_list):
+        """ Adds a ShoppingList to a specified user profile.
+        """
+
+        if shop_list.list_id in self.shoppinglists or shop_list.name in self.shoppinglist_names.values():
+            return False
+        self.shoppinglists[shop_list.list_id] = shop_list
+        self.shoppinglist_names[shop_list.list_id] = shop_list.name
+        return True
