f33817dffb6e5e7c807b07b3f2f644997d6af01d
isheebo <kedgar751@gmail.com>
2017-08-23 04:32:41 +0300

    Add test for adding a duplicate item to shoppinglist

--- tests/test_shoppinglist.py
+++ tests/test_shoppinglist.py
@@ -18,3 +18,13 @@ class TestShoppingList(unittest.TestCase):
                          "The number has just been incremented by one")
         self.assertEqual(self.shoppinglist.date_modified,
                          datetime.now().strftime("%Y-%m-%d %H:%M"))
+
+    def test_add_duplicate_item_to_shopping_list(self):
+        self.shoppinglist.add_item(Item("1243", "Cabbages", 2000, 4))
+        self.assertFalse(self.shoppinglist.add_item(
+            Item("1243", "Cabbages", 3000, 5)), "Duplicate items not allowed")
+        self.assertEqual(len(self.shoppinglist.items), 1,
+                         "The shopping list has just one item")
+        self.shoppinglist.add_item(Item("4553", "Carrots", 4000, 10))
+        self.assertEqual(len(self.shoppinglist.items), 2,
+                         "The items in the list must now be two")
