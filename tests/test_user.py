fff228444dc33c206896372f2701c3acc484afcd
isheebo <kedgar751@gmail.com>
2017-08-23 04:48:40 +0300

    FIXES: PEP 8: line too long errors

--- tests/test_user.py
+++ tests/test_user.py
@@ -42,7 +42,8 @@ class TestUser(unittest.TestCase):
         self.user.create_shoppinglist(ShoppingList(
             "HKUA452SA", "Vegetables", "2017-08-27"))
         self.assertEqual(self.user.shoppinglists["HKUA452SA"].name,
-                         "Vegetables", "Name of shopping list must be Vegetables")
+                         "Vegetables",
+                         "Name of shopping list must be Vegetables")
         self.assertEqual(
             self.user.shoppinglists["HKUA452SA"].notify_date, "2017-08-27")
 
@@ -55,13 +56,14 @@ class TestUser(unittest.TestCase):
 
         self.user.edit_shoppinglist("HKUA452SA", "Clothes", "2017-09-10")
         self.assertEqual(self.user.shoppinglists["HKUA452SA"].name,
-                         "Clothes", "The shopping list name has changed to clothes")
+                         "Clothes",
+                         "The shopping list name has changed to clothes")
         self.assertEqual(self.user.shoppinglists["HKUA452SA"].notify_date,
                          "2017-09-10", "The notify date must be 2017-09-10")
 
-        # Trying to edit a non-existent shopping list should return False. In this case,
-        # We don't have a shopping list with an ID HshfnaA and neither do we have
-        # any list called beans.
+        # Trying to edit a non-existent shopping list should return False. In
+        # this case, We don't have a shopping list with an ID HshfnaA and
+        # neither do we have any list called beans.
         self.assertFalse(self.user.edit_shoppinglist(
             "HshfnaA", "Beans", "2018-01-01"))
         self.assertEqual(self.user.shoppinglists["HKUA452SA"].name,
