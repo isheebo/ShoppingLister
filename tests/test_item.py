d32d98dd62ff3beea7ebdd69b929bd82cf0c3ef4
isheebo <kedgar751@gmail.com>
2017-08-23 04:26:14 +0300

    Add test for item name

--- tests/test_item.py
+++ tests/test_item.py
@@ -8,3 +8,10 @@ class TestItem(unittest.TestCase):
 
     def test_item_id(self):
         self.assertEqual(self.item.item_id, "1345", "item ID must be 1345")
+
+    def test_name_of_item(self):
+        self.assertEqual(self.item.name, "Cabbages",
+                         "name of the item must be Cabbages.")
+        self.item.name = "Carrots"
+        self.assertEqual(self.item.name, "Carrots",
+                         "name of the item has now changed to Carrots")
