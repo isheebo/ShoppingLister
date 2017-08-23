3d233e56cf6355fe4aa072795054e9e4bfd12f00
isheebo <kedgar751@gmail.com>
2017-08-23 11:59:22 +0300

    Modify Proc file and thr run.py file for Heroku deployment

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from models.views import lister
 app = Flask(__name__)
 app.register_blueprint(lister)
 app.secret_key = os.urandom(64)
-
+'''
 if __name__ == '__main__':
     app.run(debug=True)
+'''
