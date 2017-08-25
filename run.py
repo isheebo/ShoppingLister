488efd4532c46430126fbf38af9e2eb330887719
isheebo <kedgar751@gmail.com>
2017-08-25 11:41:44 +0300

    modify all things

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 import os
 from flask import Flask
-from models.views import lister
-
-app = Flask(__name__)
-app.register_blueprint(lister)
-app.secret_key = os.urandom(64)
+from shoppinglister import app
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ tests/test_views.py
@@ -0,0 +1,33 @@
+import unittest
+import shoppinglister.views
+from shoppinglister.models.user import User
+from shoppinglister.views import lister
+from flask import url_for, Response
+
+
+class ViewsTests(unittest.TestCase):
+    """ Contains tests for the views """
+
+    view = shoppinglister.views
+
+    def test_page_redirects_when_not_logged_in(self):
+        """ Tests whether going to shopping_list without logging in
+         redirects to the signin page """
+
+        with shoppinglister.app.test_request_context():
+            self.assertEqual(
+                shoppinglister.views.shopping_list().status_code, 302)
+            self.assertEqual(shoppinglister.views.shopping_list().location,
+                             url_for('lister.login'))
+
+    def test_logout(self):
+        """ Tests whether the loguot loads and redirects to login """
+
+        with shoppinglister.app.test_request_context():
+            self.assertEqual(shoppinglister.views.logout().status_code, 302)
+            self.assertEqual(shoppinglister.views.logout().location,
+                             url_for('lister.signup'))
+
+
+if __name__ == '__main__':
+    unittest.main()
